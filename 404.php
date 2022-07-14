2446460400fb8a17f63b4af82aee22f28561cdf1
AlexeiKhomitsa <92182450+AlexeiKhomitsa@users.noreply.github.com>
2022-07-14 18:36:41 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>PHP web-site</title>
+	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+	<?php require "blocks/header.php" ?>
+
+	<div class="container">
+		<h3 class="mb-5">Ошибка 404</h3>
+	</div>
+
+	<?php require "blocks/footer.php" ?>
+
+</body>
+
+</html>
